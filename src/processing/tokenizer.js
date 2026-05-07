ce88fde064231119b7bb0cc8b4bc583a61f9a08b
Sumit Ranjan Bahalia <165286043+PrimeSumit@users.noreply.github.com>
2026-05-07 21:43:36 +0530

    token cleanup

--- src/processing/tokenizer.js
+++ src/processing/tokenizer.js
@@ -2,8 +2,8 @@ import tools from "../../data/tools_clean.json" with { type: "json" };
 
 const text = "zoom!!! meeting-app??";
 function tokentize(text) {
-  const token = text.toLowerCase().replace(/[^a-z0-9 ]/g,"").split(" ");
-
+  const token = text.toLowerCase().replace(/[^a-z0-9 ]/g," ").split(" ").filter(word=>word!=="");
+  
   return token;
 }
 const result = tokentize(text);
