d5fa70ddc27cc06479cec8adb5400ba4380946aa
Sumit Ranjan Bahalia <165286043+PrimeSumit@users.noreply.github.com>
2026-05-07 20:33:55 +0530

    add normalization in tokenizer

--- src/processing/tokenizer.js
+++ src/processing/tokenizer.js
@@ -1,8 +1,8 @@
 import tools from "../../data/tools_clean.json" with { type: "json" };
 
-const text = "zoom meeting app";
+const text = "zoom!!! meeting-app??";
 function tokentize(text) {
-  const token = text.toLowerCase().split(" ");
+  const token = text.toLowerCase().replace(/[^a-z0-9 ]/g,"").split(" ");
 
   return token;
 }
