3525d92740c5e8a1b80ff4c45356cd79d226bed9
Sumit Ranjan Bahalia <165286043+PrimeSumit@users.noreply.github.com>
2026-05-10 21:58:25 +0530

    basic boolean retrival engine

--- src/search/search.js
+++ src/search/search.js
@@ -1,10 +1,15 @@
 import invertedIndex from "../index/buildIndex.js";
 import tokenize from "../processing/tokenizer.js";
-
-const query = "video meetings";
+import tools from "../../data/tools_clean.json" with { type: "json" };
+const query = "zoom";
 
 const tokens = tokenize(query);
 tokens.forEach((token) => {
   const result = invertedIndex[token];
   console.log(result);
+  tools.forEach((tool) => {
+    if (result.includes(tool.id)) {
+      console.log(tool.name);
+    }
+  });
 });
