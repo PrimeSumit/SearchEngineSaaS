1c4327aaced878bcea020b2ae8432204fb46eaf8
Sumit Ranjan Bahalia <165286043+PrimeSumit@users.noreply.github.com>
2026-05-11 22:01:01 +0530

    Boolean AND Retrival

--- src/search/search.js
+++ src/search/search.js
@@ -4,13 +4,19 @@ import tools from "../../data/tools_clean.json" with { type: "json" };
 const query = "video meetings";
 
 const tokens = tokenize(query);
-//OR retrival : this is any match token print
-tokens.forEach((token) => {
-  const result = invertedIndex[token];
 
-  tools.forEach((tool) => {
-    if (result.includes(tool.id)) {
-      console.log(tool.name);
-    }
+let currentResult = invertedIndex[tokens[0]];
+tokens.forEach((token, index) => {
+  if (index === 0) {
+    return;
+  }
+  const result = invertedIndex[token];
+  currentResult = currentResult.filter((id) => {
+    return result.includes(id);
   });
+});
+tools.forEach((tool) => {
+  if (currentResult.includes(tool.id)) {
+    console.log(tool.name);
+  }
 });
