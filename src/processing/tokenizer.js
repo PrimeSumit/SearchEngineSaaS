f675b9518075da9047255ea0ddd0e49be1372831
Sumit Ranjan Bahalia <165286043+PrimeSumit@users.noreply.github.com>
2026-05-12 16:46:46 +0530

    stopword remove

--- src/processing/tokenizer.js
+++ src/processing/tokenizer.js
@@ -1,11 +1,14 @@
 import tools from "../../data/tools_clean.json" with { type: "json" };
+import stopwords from "./stopwords.js";
 
 export default function tokenize(text) {
   const token = text
     .toLowerCase()
     .replace(/[^a-z0-9 ]/g, " ")
     .split(" ")
-    .filter((word) => word !== "");
+    .filter((word) => word !== "")
+    .filter((t) => !stopwords.has(t));
 
   return token;
 }
+
