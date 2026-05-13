9001ba413248bf0911b3dd0d0c098ec1334ac679
Sumit Ranjan Bahalia <165286043+PrimeSumit@users.noreply.github.com>
2026-05-13 22:34:45 +0530

    hybrid retrival AND strict and then OR fallback

--- src/search/search.js
+++ src/search/search.js
@@ -5,18 +5,32 @@ const query = "video meetings";
 
 const tokens = tokenize(query);
 
-let currentResult = invertedIndex[tokens[0]];
+let andResult = invertedIndex[tokens[0]];
 tokens.forEach((token, index) => {
   if (index === 0) {
     return;
   }
   const result = invertedIndex[token];
-  currentResult = currentResult.filter((id) => {
+  andResult = andResult.filter((id) => {
     return result.includes(id);
   });
 });
 tools.forEach((tool) => {
-  if (currentResult.includes(tool.id)) {
+  if (andResult.includes(tool.id)) {
     console.log(tool.name);
   }
 });
+let orResult = new Set(invertedIndex[tokens[0]] || []);
+tokens.forEach((token, index) => {
+  if (index === 0) return;
+  const result = invertedIndex[token]||[];
+  result.forEach((id) => {
+    orResult.add(id);
+  });
+});
+tools.forEach((tool) => {
+  if (orResult.has(tool.id) && !andResult.includes(tool.id)){
+   console.log(tool.name)
+  }
+});
+
