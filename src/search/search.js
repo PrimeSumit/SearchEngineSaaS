b66179a02f4ebb1d51f2c52a65cd5e5cd670ef71
Sumit Ranjan Bahalia <165286043+PrimeSumit@users.noreply.github.com>
2026-05-13 22:49:38 +0530

    fixs

--- src/search/search.js
+++ src/search/search.js
@@ -20,17 +20,17 @@ tools.forEach((tool) => {
     console.log(tool.name);
   }
 });
+
 let orResult = new Set(invertedIndex[tokens[0]] || []);
 tokens.forEach((token, index) => {
   if (index === 0) return;
-  const result = invertedIndex[token]||[];
+  const result = invertedIndex[token] || [];
   result.forEach((id) => {
     orResult.add(id);
   });
 });
 tools.forEach((tool) => {
-  if (orResult.has(tool.id) && !andResult.includes(tool.id)){
-   console.log(tool.name)
+  if (orResult.has(tool.id) && !andResult.includes(tool.id)) {
+    console.log(tool.name);
   }
 });
-
