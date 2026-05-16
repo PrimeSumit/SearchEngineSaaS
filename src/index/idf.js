d382cd0f64a4974a86d7d64ad200d1f773f727b4
Sumit Ranjan Bahalia <165286043+PrimeSumit@users.noreply.github.com>
2026-05-16 23:44:19 +0530

    idf complete the rarity of token

---
+++ src/index/idf.js
@@ -0,0 +1,11 @@
+import tools from "../../data/tools_clean.json" with {type:"json"}
+import documentFrequency from "./documentFreq.js"
+
+const idf={}
+
+Object.keys(documentFrequency).forEach((token) => {
+    const n=tools.length
+    const DF=documentFrequency[token]
+    const result=Math.log(n/DF)
+    idf[token]=result
+});
