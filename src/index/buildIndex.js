01f3d565ca9e1918cebcbaca437b2da984892157
Sumit Ranjan Bahalia <165286043+PrimeSumit@users.noreply.github.com>
2026-05-18 22:53:26 +0530

    calculate tf-idf scores

--- src/index/buildIndex.js
+++ src/index/buildIndex.js
@@ -19,4 +19,4 @@ tools.forEach((tool) => {
     }
   });
 });
-console.log(invertedIndex["video"]);
+
---
+++ src/ranking/tfidf.js
@@ -0,0 +1,16 @@
+import tokenize from "../processing/tokenizer.js";
+import invertedIndex from "../index/buildIndex.js";
+import idf from "../index/idf.js";
+
+const query = "ai video";
+const tokens = tokenize(query);
+const search = {};
+export default search;
+tokens.forEach((token) => {
+  Object.entries(invertedIndex[token]).forEach((entry) => {
+    const toolId = entry[0];
+    const tf = entry[1];
+    const score = tf * idf[token];
+    console.log(score);
+  });
+});
