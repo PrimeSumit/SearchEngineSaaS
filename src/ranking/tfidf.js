430f30c6ea39e8a95b0cb8a06660485956aec3fa
Sumit Ranjan Bahalia <165286043+PrimeSumit@users.noreply.github.com>
2026-05-20 23:12:55 +0530

    import for tools lookup from the dataset

--- src/ranking/tfidf.js
+++ src/ranking/tfidf.js
@@ -1,6 +1,7 @@
 import tokenize from "../processing/tokenizer.js";
 import invertedIndex from "../index/buildIndex.js";
 import idf from "../index/idf.js";
+import tools from "../../data/tools_clean.json" with { type: "json" };
 
 const query = "video meetings";
 const tokens = tokenize(query);
